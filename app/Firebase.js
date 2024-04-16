c539e55cba09afef8771e5623afb460d022c6f51
Roman Ryazantsev <roman.a.ryazantsev@gmail.com>
2024-04-16 15:10:00 +0300

    no message

---
+++ app/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA9gyR2Vzlx4Wbp0nLY7JnTGPwYk2rkxMQ",
+    authDomain: "scrum-manager-e0978.firebaseapp.com",
+    projectId: "scrum-manager-e0978",
+    storageBucket: "scrum-manager-e0978.appspot.com",
+    messagingSenderId: "116287469649",
+    appId: "1:116287469649:web:582bb4f590465ef21d6070"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div>
+            <text> hello world </text>
+        </div>
+    )
+}
